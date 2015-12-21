ecf095778cbad6a430bb677613bc1c9a11785a3c
liaklein <lklein13@u.rochester.edu>
2015-12-20 20:05:58 -0500

    now you can safe rm multiple files at once, undo undoes all of the

--- safe_rm.py
+++ safe_rm.py
@@ -8,31 +8,35 @@ import time
 
 #there are two important pieces of information: where the script (or more importantly, the trash folder) is located, and where the file you are deleting is located
 #since I am on my system, I know the location of the trash folder. for now I can hard code that in. But for someone else who may want to download my script, I will have to provide a setup script or something that just runs once when they first download so that it will make the trash folder in the location that they want it
+#make it so that it works with an optional number of arguments
+#I think we don't even need to use the index
 def path_to_name(date,path):
     #replace / with _
     return str(date) + path.replace('/','_')
 
-def main(local):
+def main(files):
     #first you need to find the location of the script itelf. weird
     me =  os.path.realpath(__file__)
     mydir = os.path.dirname(me) + "/"
     
-    #then you need to find the current working directory
-    filepath = os.path.realpath(local)
-    
     #get the date and time
     ts = time.time()
     date = dt.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+
+    #index = open(mydir+".index",'a')
+
+    for f in files:
+        #then you need to find the full file path
+        filepath = os.path.realpath(f)
     
-    #write the proper information to the index
-    index = open(mydir+".index",'a')
-    index.write(date + " " + filepath+"\n")
+        #write the proper information to the index
+        #index.write(date + " " + filepath+"\n")
     
-    #do the move
-    command = "mv " + local + " " + mydir + "trash/" + path_to_name(date,filepath)
-    sp.call(command.split())
+        #do the move
+        command = "mv " + filepath + " " + mydir + "trash/" + path_to_name(date,filepath)
+        sp.call(command.split())
 
 if __name__=="__main__":
-    main(sys.argv[1])
+    main(sys.argv[1:])
 
 
--- undo.py
+++ undo.py
@@ -3,12 +3,13 @@ import sys
 import os
 import argparse
 import subprocess as sp
+import glob
 
 def path_to_name(date,path):
     #replace / with _
     return str(date) + path.replace('/','_')
 
-def main():
+def oldmain():
     #first you need to find the location of the script itelf. weird
     me =  os.path.realpath(__file__)
     mydir = os.path.dirname(me) + "/"
@@ -35,6 +36,51 @@ def main():
     #move it back
     command = "mv "+ mydir+"/trash/" + fname + " " + filepath
     sp.call(command.split())
+
+def latest(files,trashdir):
+    lastfile = files[len(files)-1]
+    date = lastfile.split('_')[0]
+
+    command = "ls " + trashdir + date + "*"
+    pattern = trashdir + date + "*"
+    print command
+    
+    #latest_files = sp.check_output(command.split()).split()
+    latest_files = glob.glob(pattern)
+    return latest_files
+
+def move(f,trashdir):
+    #first turn filename in trash to the location you want to move it
+    print "f in move: " + f
+    parts = f.split('/')
+    last = parts[len(parts) - 1]
+    name = last.replace('_','/')
+    print "name: " + name
+    namelist = name.split('/')[1:]
+    print "namelist: " + str(namelist)
+    fname = "/" + '/'.join(namelist)
+
+    print "fname: " + fname
+
+    #then do the move
+    command = "mv " + f + " " + fname
+    print "command: " + command
+    sp.call(command.split())
+
+def main():
+    #first you need to find the location of the script itelf. weird
+    me =  os.path.realpath(__file__)
+    mydir = os.path.dirname(me) + "/"
+    trashdir = mydir + "trash/"
+    
+    #figure out which files you are moving back by getting the list of files with the most recent date
+    command = "ls " + trashdir
+    files = sp.check_output(command.split()).split()
+    movefiles = latest(files,trashdir)
+
+    for f in movefiles:
+        move(f,trashdir)
+
 
 if __name__=="__main__":
     main()
